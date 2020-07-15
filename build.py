952c968a067d63ca1dc2e0f735e3031db471406c
Oscar Bernal <oscarbernal93@gmail.com>
2020-07-15 16:39:55 -0500

    Adds the change log to build function
    Signed-off-by: Oscar Bernal <oscarbernal93@gmail.com>

--- build.py
+++ build.py
@@ -7,25 +7,74 @@ config.read("config.ini")
 title = config.get("header","title")
 subtitle = config.get("header","subtitle")
 
-outputFile = open("out/index.html", "w")
-
 headerFile = open("partial/header.html","r")
 header = "".join(headerFile.readlines())
 
 footerFile = open("partial/footer.html","r")
 footer = "".join(footerFile.readlines())
 
-outputFile.write(header)
-
-outputFile.write(f"<title>{title}</title>\n")
-outputFile.write(f"<h1>{title}</h1>\n")
-outputFile.write(f"<h3>{subtitle}</h3>\n")
-outputFile.write(f"</head>\n")
-
 contentFile = open("content/index.html","r")
 content = "".join(contentFile.readlines())
 
+#start of index page
+
+outputFile = open("out/index.html", "w")
+
+outputFile.write(header)
+
+outputFile.write(f"<title>{title}</title>\n")
+outputFile.write(f"</head>\n")
 outputFile.write(f"<body>\n")
+
+outputFile.write(f"<h1>{title}</h1>\n")
+outputFile.write(f"<h3>{subtitle}</h3>\n")
+
 outputFile.write(content)
 
 outputFile.write(footer)
+
+#end of index page
+
+#start of changelog
+
+outputFile = open("out/changes.html", "w")
+
+outputFile.write(header)
+
+outputFile.write(f"<title>{title} - Changelog</title>\n")
+outputFile.write(f"</head>\n")
+outputFile.write(f"<body>\n")
+
+outputFile.write(f"<h1>{title}</h1>\n")
+outputFile.write(f"<h3>{subtitle}</h3>\n")
+
+outputFile.write("<table>\n")
+outputFile.write("<tr>\n")
+outputFile.write("<th>Version<th>\n")
+outputFile.write("<th>Date<th>\n")
+outputFile.write("<th>Changes<th>\n")
+outputFile.write("</tr>\n")
+
+
+#start of dinamicly generate table
+
+changelogFile = open("changelog.txt", "r")
+
+for change in changelogFile.readlines():
+    parts = change.split("|")
+    vers = parts[0]
+    date = parts[1]
+    desc = parts[2]
+    
+    outputFile.write("<tr>\n")
+    outputFile.write(f"<td>{vers}</td>\n")
+    outputFile.write(f"<td>{date}</td>\n")
+    outputFile.write(f"<td>{desc}</td>\n")
+    outputFile.write("</tr>\n")
+
+#end of dinamic table
+outputFile.write(f"</table>\n")
+
+outputFile.write(footer)
+
+#end of changelog
