eeb7a3d8e1c83f27dfcc064144a5f25de1b84acb
Oscar Bernal <oscarbernal93@gmail.com>
2020-08-16 10:02:33 -0500

    Update Content

--- publish.sh
+++ publish.sh
@@ -11,12 +11,14 @@ git for-each-ref --sort=-refname --format="$FORMAT" refs/tags > changelog.txt
 python3 build.py
 
 cp -r assets/* out/
+cp -r out/* ../ZeroNet/my_site/
+
+cp -r out/* 19NW5k68A3qRsomy42hLBZFsHbQ6kFg2yN/
+
+git commit -am "Update site mirror"
 
 git push
 
 git push --tags
-
-cp -r out/* ../ZeroNet/my_site/
-cp -r out/* 19NW5k68A3qRsomy42hLBZFsHbQ6kFg2yN/
 
 echo "Done!"
